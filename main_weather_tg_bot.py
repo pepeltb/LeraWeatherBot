158dd6f8aa6056c452be725b336650250b18c291
pepeltb <89778720+pepeltb@users.noreply.github.com>
2023-01-22 23:28:56 +0300

    Update main_weather_tg_bot.py

--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -25,45 +25,33 @@ async def get_weather(message: types.Message):
     }
 
     try:
-        cur_weather_check = 0
-        humidity_check = 0
-        speed_check = 0
-        emergency_brake = True
-        while emergency_brake:
-            pogoda = r.get(f'https://api.openweathermap.org/data/2.5/weather?lat=56.50&lon=60.35&appid={open_weather_token}&units=metric')
-            data = pogoda.json()
-            #print(data)
+        
+        pogoda = r.get(f'https://api.openweathermap.org/data/2.5/weather?lat=56.50&lon=60.35&appid={open_weather_token}&units=metric')
+        data = pogoda.json()
+        #print(data)
 
-            cur_weather = data['main']['temp']
-            humidity = data['main']['humidity']
-            speed = data['wind']['speed']
+        cur_weather = data['main']['temp']
+        humidity = data['main']['humidity']
+        speed = data['wind']['speed']
 
 
-            weather_description = data["weather"][0]["main"]
-            if weather_description in code_to_smile:
-                wd = code_to_smile[weather_description]
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
 
 
-            message = (f'*** Сегодня {datetime.now().strftime("%d.%m.%Y")} ***\n'
-                    f'В Екатеринбурге: {cur_weather}°, {wd}\n'
-                    f'Влажность в штанах: {humidity}%\n'
-                    f'Скорость вiтра тобi у сраку: {round((speed * 3.6), 1)} км/ч')
+        message = (f'*** Сегодня {datetime.now().strftime("%d.%m.%Y")} ***\n'
+                f'В Екатеринбурге: {cur_weather}°, {wd}\n'
+                f'Влажность в штанах: {humidity}%\n'
+                f'Скорость вiтра тобi у сраку: {round((speed * 3.6), 1)} км/ч')
 
-            if cur_weather == cur_weather_check and humidity == humidity_check and speed == speed_check:
-                await bot.send_message(CHANNEL_NAME, 'Токен протух, а ты - петух')
-                emergency_brake = False
-                break
+        
 
 
+        #print(message)
+        await bot.send_message(CHANNEL_NAME, message)
 
-            #print(message)
-            await bot.send_message(CHANNEL_NAME, message)
-
-            cur_weather_check = cur_weather
-            humidity_check = humidity
-            speed_check = speed
-
-            time.sleep(86400)
+            
 
 
 
