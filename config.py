934b636bff3523720d7c5afe2e77b3563f81d5fe
pepeltb <89778720+pepeltb@users.noreply.github.com>
2023-01-22 23:44:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 open_weather_token = 'b112a90a734ceced194375d32998a375'
-tg_bot_token = "5940640986:AAHmvD1RKEzsHSSnbqQuiY4rsO1Xl6zqsYY"
+tg_bot_token = "5940640986:AAHe2XALsA8WHARuhKXfUA01wnyitikv8N4"
